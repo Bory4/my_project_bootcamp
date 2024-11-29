1e0ee471bedbdc991b764761a6dd3921b73e2dd4
Bory 4 <krzysztof.borowka@o2.pl>
2024-11-29 12:29:21 +0000

    styling tailwind

--- src/my_project_frontend/src/App.vue
+++ src/my_project_frontend/src/App.vue
@@ -22,7 +22,6 @@ getChat()
   <main class="container mx-auto flex justify-center items-center flex-col p-6 border-2 border-cyan-700 my-6 w-fit rounded-xl">
     <img src="/logo2.svg" alt="DFINITY logo" />
     <br />
-    <br />
     <form action="#" @submit="handleSubmit">
       <label for="msg">Enter your message: &nbsp;</label>
       <input id="msg" alt="msg" type="text" />
